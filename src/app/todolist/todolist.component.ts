27d92d5ec24772b978709beaccb1f243147251e0
vishwanathr <vishwanath.reddy@remiges.tech>
2024-05-14 15:12:32 +0530

    New

---
+++ src/app/todolist/todolist.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrl: './todolist.component.css'
+})
+export class TodolistComponent {
+
+  taskArray = [{ taskName: 'Brush teeth', isCompleted: false, isEditable: false }];
+
+  constructor() { }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+    this.taskArray.push({
+      taskName: form.controls['task'].value,
+      isCompleted: false,
+      isEditable: false
+    })
+    form.reset();
+  }
+
+  onDelete(index: number){
+    console.log(index);
+    this.taskArray.splice(index, 1);
+  }
+
+  onCheck(index: number){
+    console.log(this.taskArray);
+    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+  }
+
+  onEdit(index: number){
+    this.taskArray[index].isEditable = true;
+  }
+
+  onSave(index: number, newTask: string){
+    this.taskArray[index].taskName = newTask;
+    this.taskArray[index].isEditable = false;
+  }
+}
